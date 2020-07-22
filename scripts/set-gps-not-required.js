4d80395949478b8cd3c6fd505ee4142d5830ba33
Ahmed Galal <a.galal.atia@gmail.com>
2020-07-22 06:27:15 +0200

    update set-gps file

--- scripts/set-gps-not-required.js
+++ scripts/set-gps-not-required.js
@@ -1,75 +1,75 @@
 #!/usr/bin/env node
-//
-// module.exports = function(context) {
-//
-//   var fs = require('fs'),
-//       path = require('path');
-//
-//   var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-//   var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-//
-//   console.log("Platform ROOT: " + platformRoot);
-//   console.log("Manifest file: " + manifestFile);
-//
-//   // If manifest file exists
-//   if (fs.existsSync(manifestFile)) {
-//     console.log("Manifest file exists");
-//
-//     // Read manifest file
-//     fs.readFile(manifestFile, 'utf8', function (err, data) {
-//       if (err) {
-//         throw new Error('Unable to find AndroidManifest.xml: ' + err);
-//       }
-//
-//       // Remove any current GPS settings
-//       data = data.replace(/<uses-feature android:name="android.hardware.location.gps" \/>/g, '');
-//       data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>/g, '');
-//
-//       // Add GPS uses-feature (setting to required false)
-//       data = data.replace("</manifest>",
-//         '\t<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>\n</manifest>');
-//
-//       // Replace manifest file with updated version
-//       if(data){
-//         fs.writeFile(manifestFile, data, 'utf8', function (err) {
-//           if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
-//         })
-//       }
-//     });
-//   } else {
-//     console.log("Manifest file DOES NOT exist");
-//   }
-// };
-var fs = require('fs'),
-    path = require('path');
-// console.log(path)
 
-// var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-// var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-var manifestFile = 'android/capacitor-cordova-android-plugins/src/main/AndroidManifest.xml';
+module.exports = function(context) {
 
+  var fs = require('fs'),
+      path = require('path');
 
-// console.log("Platform ROOT: " + platformRoot);
-console.log("Manifest file: " + manifestFile);
+  var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+  var manifestFile = path.join(platformRoot, 'capacitor-cordova-android-plugins/src/main/AndroidManifest.xml');
 
-// If manifest file exists
-if (fs.existsSync(manifestFile)) {
+  console.log("Platform ROOT: " + platformRoot);
+  console.log("Manifest file: " + manifestFile);
+
+  // If manifest file exists
+  if (fs.existsSync(manifestFile)) {
     console.log("Manifest file exists");
 
     // Read manifest file
     fs.readFile(manifestFile, 'utf8', function (err, data) {
-        if (err) {
-            throw new Error('Unable to find AndroidManifest.xml: ' + err);
-        }
-        data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+      if (err) {
+        throw new Error('Unable to find AndroidManifest.xml: ' + err);
+      }
 
-        // Replace manifest file with updated version
-        if (data) {
-            fs.writeFile(manifestFile, data, 'utf8', function (err) {
-                if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
-            })
-        }
+      data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+
+
+      // // Remove any current GPS settings
+      // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" \/>/g, '');
+      // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>/g, '');
+      //
+      // // Add GPS uses-feature (setting to required false)
+      // data = data.replace("</manifest>",
+      //   '\t<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>\n</manifest>');
+
+      // Replace manifest file with updated version
+      if(data){
+        fs.writeFile(manifestFile, data, 'utf8', function (err) {
+          if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
+        })
+      }
     });
-} else {
+  } else {
     console.log("Manifest file DOES NOT exist");
-}
+  }
+};
+
+// var fs = require('fs')
+//
+// var manifestFile = '/builds/Ahmed-Galal/moodleapp/android/capacitor-cordova-android-plugins/src/main/AndroidManifest.xml';
+//
+//
+// // console.log("Platform ROOT: " + platformRoot);
+// console.log("Manifest file: " + manifestFile);
+//
+// // If manifest file exists
+// if (fs.existsSync(manifestFile)) {
+//     console.log("Manifest file exists");
+//
+//     // Read manifest file
+//     fs.readFile(manifestFile, 'utf8', function (err, data) {
+//         if (err) {
+//             throw new Error('Unable to find AndroidManifest.xml: ' + err);
+//         }
+//         data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+//
+//         // Replace manifest file with updated version
+//         if (data) {
+//             fs.writeFile(manifestFile, data, 'utf8', function (err) {
+//                 if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
+//             })
+//         }
+//     });
+// } else {
+//     console.log("Manifest file DOES NOT exist");
+// }
--- set-gps-not-required.js
+++ set-gps-not-required.js
@@ -1,55 +1,14 @@
 #!/usr/bin/env node
-//
-// module.exports = function(context) {
-//
-//   var fs = require('fs'),
-//       path = require('path');
-//
-//   var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-//   var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-//
-//   console.log("Platform ROOT: " + platformRoot);
-//   console.log("Manifest file: " + manifestFile);
-//
-//   // If manifest file exists
-//   if (fs.existsSync(manifestFile)) {
-//     console.log("Manifest file exists");
-//
-//     // Read manifest file
-//     fs.readFile(manifestFile, 'utf8', function (err, data) {
-//       if (err) {
-//         throw new Error('Unable to find AndroidManifest.xml: ' + err);
-//       }
-//
-//       // Remove any current GPS settings
-//       data = data.replace(/<uses-feature android:name="android.hardware.location.gps" \/>/g, '');
-//       data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>/g, '');
-//
-//       // Add GPS uses-feature (setting to required false)
-//       data = data.replace("</manifest>",
-//         '\t<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>\n</manifest>');
-//
-//       // Replace manifest file with updated version
-//       if(data){
-//         fs.writeFile(manifestFile, data, 'utf8', function (err) {
-//           if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
-//         })
-//       }
-//     });
-//   } else {
-//     console.log("Manifest file DOES NOT exist");
-//   }
-// };
+
+
 var fs = require('fs'),
     path = require('path');
-// console.log(path)
 
-// var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-// var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-var manifestFile = '/builds/Ahmed-Galal/moodleapp/android/capacitor-cordova-android-plugins/src/main/AndroidManifest.xml';
+var appDir = path.dirname(require.main.filename);
 
+var manifestFile = path.join(appDir, 'capacitor-cordova-android-plugins/src/main/AndroidManifest.xml');
 
-// console.log("Platform ROOT: " + platformRoot);
+console.log("APP ROOT: " + appDir);
 console.log("Manifest file: " + manifestFile);
 
 // If manifest file exists
@@ -61,7 +20,17 @@ if (fs.existsSync(manifestFile)) {
         if (err) {
             throw new Error('Unable to find AndroidManifest.xml: ' + err);
         }
+
         data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+
+
+        // // Remove any current GPS settings
+        // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" \/>/g, '');
+        // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>/g, '');
+        //
+        // // Add GPS uses-feature (setting to required false)
+        // data = data.replace("</manifest>",
+        //   '\t<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>\n</manifest>');
 
         // Replace manifest file with updated version
         if (data) {
@@ -73,3 +42,80 @@ if (fs.existsSync(manifestFile)) {
 } else {
     console.log("Manifest file DOES NOT exist");
 }
+
+
+// module.exports = function (context) {
+//
+//     var fs = require('fs'),
+//         path = require('path');
+//
+//     var appDir = path.dirname(require.main.filename);
+//
+//     // var platformRoot = path.join(context.opts.projectRoot, 'android');
+//     var manifestFile = path.join(appDir, 'capacitor-cordova-android-plugins/src/main/AndroidManifest.xml');
+//
+//     console.log("APP ROOT: " + appDir);
+//     console.log("Manifest file: " + manifestFile);
+//
+//     // If manifest file exists
+//     if (fs.existsSync(manifestFile)) {
+//         console.log("Manifest file exists");
+//
+//         // Read manifest file
+//         fs.readFile(manifestFile, 'utf8', function (err, data) {
+//             if (err) {
+//                 throw new Error('Unable to find AndroidManifest.xml: ' + err);
+//             }
+//
+//             data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+//
+//
+//             // // Remove any current GPS settings
+//             // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" \/>/g, '');
+//             // data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>/g, '');
+//             //
+//             // // Add GPS uses-feature (setting to required false)
+//             // data = data.replace("</manifest>",
+//             //   '\t<uses-feature android:name="android.hardware.location.gps" android:required="false" \/>\n</manifest>');
+//
+//             // Replace manifest file with updated version
+//             if (data) {
+//                 fs.writeFile(manifestFile, data, 'utf8', function (err) {
+//                     if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
+//                 })
+//             }
+//         });
+//     } else {
+//         console.log("Manifest file DOES NOT exist");
+//     }
+// };
+
+// var fs = require('fs')
+//
+// var manifestFile = '/builds/Ahmed-Galal/moodleapp/android/capacitor-cordova-android-plugins/src/main/AndroidManifest.xml';
+//
+//
+// // console.log("Platform ROOT: " + platformRoot);
+// console.log("Manifest file: " + manifestFile);
+//
+// // If manifest file exists
+// if (fs.existsSync(manifestFile)) {
+//     console.log("Manifest file exists");
+//
+//     // Read manifest file
+//     fs.readFile(manifestFile, 'utf8', function (err, data) {
+//         if (err) {
+//             throw new Error('Unable to find AndroidManifest.xml: ' + err);
+//         }
+//         data = data.replace(/<uses-feature android:name="android.hardware.location.gps" android:required="\$GPS_REQUIRED"\/>/g, '<uses-feature android:name="android.hardware.location.gps" android:required="false"/>')
+//
+//         // Replace manifest file with updated version
+//         if (data) {
+//             fs.writeFile(manifestFile, data, 'utf8', function (err) {
+//                 if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
+//             })
+//         }
+//     });
+// } else {
+//     console.log("Manifest file DOES NOT exist");
+// }
